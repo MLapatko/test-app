7360dedc1607b0b314c18adaed57ae61825df0c4
Dima <dima22.dima22@mail.ru>
2017-06-15 16:05:26 +0300

    Create main screen

--- app/src/main/java/by/lovata/a2doc/MainActivity.java
+++ app/src/main/java/by/lovata/a2doc/MainActivity.java
@@ -1,54 +1,94 @@
 package by.lovata.a2doc;
 
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
-import by.lovata.a2doc.tabScreens.SlidingTabLayout;
-import by.lovata.a2doc.tabScreens.ViewPagerAdapter;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends ActionBarActivity {
+import by.lovata.a2doc.aboutScreen.TabAboutFragment;
+import by.lovata.a2doc.screenCities.TabCitiesFragment;
+import by.lovata.a2doc.screenQuestion.TabQuestionFragment;
+import by.lovata.a2doc.screenSearch.TabSearchFragment;
 
-    Toolbar toolbar;
-    ViewPager pager;
-    ViewPagerAdapter adapter;
-    SlidingTabLayout tabs;
-    CharSequence Titles[]={"1", "2", "3", "4"};
-    int Numboftabs = 4;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private int[] tabIcons = {
+            R.mipmap.ic_launcher,
+            R.mipmap.ic_launcher,
+            R.mipmap.ic_launcher,
+            R.mipmap.ic_launcher
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
 
-        toolbar = (Toolbar) findViewById(R.id.tool_bar);
-        setSupportActionBar(toolbar);
-
-
-        adapter =  new ViewPagerAdapter(getSupportFragmentManager(),Titles,Numboftabs);
-
-        pager = (ViewPager) findViewById(R.id.pager);
-        pager.setAdapter(adapter);
-
-        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
-        tabs.setDistributeEvenly(true);
-
-        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
-            @Override
-            public int getIndicatorColor(int position) {
-                return getResources().getColor(R.color.tabsScrollColor);
-            }
-        });
-
-        tabs.setViewPager(pager);
-
-
-
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setSelectedTabIndicatorColor(getResources().getColor(R.color.colorPrimary));
+        setupTabIcons();
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new TabSearchFragment(), "ONE");
+        adapter.addFragment(new TabAboutFragment(), "TWO");
+        adapter.addFragment(new TabQuestionFragment(), "THREE");
+        adapter.addFragment(new TabCitiesFragment(), "FOUR");
+        viewPager.setAdapter(adapter);
+    }
+
+    private void setupTabIcons() {
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
+        tabLayout.getTabAt(3).setIcon(tabIcons[3]);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return null;
+        }
+    }
+
 
 
 }
--- app/src/main/java/by/lovata/a2doc/screenSearch/TabSearchFragment.java
+++ app/src/main/java/by/lovata/a2doc/screenSearch/TabSearchFragment.java
@@ -1,30 +1,40 @@
 package by.lovata.a2doc.screenSearch;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import by.lovata.a2doc.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class TabSearchFragment extends Fragment {
 
 
     public TabSearchFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab_search, container, false);
+        View root_view = inflater.inflate(R.layout.fragment_tab_search, container, false);
+
+        Button btn_search = (Button) root_view.findViewById(R.id.search_fragment_btn_search);
+        btn_search.setOnClickListener(clickListener_btn_profession);
+
+        return root_view;
     }
+
+    View.OnClickListener clickListener_btn_profession = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getContext(), ListProfessionActivity.class);
+            startActivity(intent);
+        }
+    };
 
 }
