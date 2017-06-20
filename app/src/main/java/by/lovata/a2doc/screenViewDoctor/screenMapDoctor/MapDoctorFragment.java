ecb0fdba608140d5e090ee08403323c668d15a17
Anton Shestak <anton.shestak.96@gmail.com>
2017-06-20 21:51:40 +0300

    change yandex maps on google and add start map

--- app/src/main/java/by/lovata/a2doc/screenViewDoctor/screenMapDoctor/MapDoctorFragment.java
+++ app/src/main/java/by/lovata/a2doc/screenViewDoctor/screenMapDoctor/MapDoctorFragment.java
@@ -1,37 +1,38 @@
 package by.lovata.a2doc.screenViewDoctor.screenMapDoctor;
 
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.location.Address;
-import android.location.Geocoder;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-import by.lovata.a2doc.LogoActivity;
 import by.lovata.a2doc.R;
 import by.lovata.a2doc.screenStart.MainActivity;
 import by.lovata.a2doc.screenViewDoctor.DoctorInfo;
 import by.lovata.a2doc.screenViewDoctor.InterfaceDoctors;
-import ru.yandex.yandexmapkit.MapController;
-import ru.yandex.yandexmapkit.MapView;
-import ru.yandex.yandexmapkit.utils.GeoPoint;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapDoctorFragment extends Fragment {
+public class MapDoctorFragment extends Fragment implements OnMapReadyCallback {
 
     DoctorInfo[] doctorsInfo;
     InterfaceDoctors doctorsInterface;
+    MapView mMapView;
+    GoogleMap gMap;
+    private FragmentActivity myContext;
 
     public MapDoctorFragment() {
         // Required empty public constructor
@@ -45,37 +46,36 @@ public class MapDoctorFragment extends Fragment {
         doctorsInfo = doctorsInterface.getDoctors();
 
         View view = inflater.inflate(R.layout.fragment_map_doctor, container, false);
+        mMapView = (MapView) view.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.onResume();// needed to get the map to display immediately
+
         SharedPreferences sharedPreferences = getActivity().getSharedPreferences(
                 MainActivity.NAME_PREFERENCES, Context.MODE_PRIVATE);
 
-        final MapView mMapView = (MapView) view.findViewById(R.id.map);
-
-        // Получаем MapController
-        MapController mMapController = mMapView.getMapController();
-
-        // Перемещаем карту на заданные координаты
-        Geocoder geocoder = new Geocoder(getActivity());
         try {
-            Map<Integer, String> cities = LogoActivity.getCities();
-            List<Address> addresses = geocoder.getFromLocationName(
-                    cities.get(sharedPreferences.getInt(
-                            MainActivity.CITY_SELECT, 0)), 1);
-            GeoPoint point = new GeoPoint(addresses.get(0).getLatitude(), addresses.get(0)
-                    .getLongitude());
-            mMapController.setPositionAnimationTo(point);
-            mMapController.setZoomCurrent(15);
-        } catch (IOException e) {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
+        mMapView.getMapAsync(this);
         return view;
     }
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
+        myContext = (FragmentActivity) activity;
         this.doctorsInterface = (InterfaceDoctors) activity;
     }
 
-
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMap = googleMap;
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        gMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        gMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 }
